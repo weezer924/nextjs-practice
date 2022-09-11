28560ff3ae1fae805336825694bf6795ce067fe2
Jakushin Cho <weezer970@gmail.com>
2022-09-11 11:14:43 +0900

    js version

---
+++ components/data.js
@@ -0,0 +1,5 @@
+export default [
+  {"name": "taro", "mail": "taro@yaamada", "age": 39},
+  {"name": "taro2", "mail": "taro2@yaamada", "age": 29},
+  {"name": "taro3", "mail": "taro3@yaamada", "age": 59},
+]
